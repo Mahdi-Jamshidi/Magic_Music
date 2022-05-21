b61bd84546e23d294d49eaaef55214f69ee7bfeb
MagicCodes <magiccodes1@gmail.com>
2022-05-21 12:59:12 +0430

    Complete slider and sound timer text

--- app/src/main/java/ir/magiccodes/magicmusic/MainActivity.kt
+++ app/src/main/java/ir/magiccodes/magicmusic/MainActivity.kt
@@ -4,12 +4,15 @@ import android.media.MediaParser
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.slider.Slider
 import ir.magiccodes.magicmusic.databinding.ActivityMainBinding
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var mediaPlayer: MediaPlayer
     var isPlaying = true
+    var isUserChanging = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +25,21 @@ class MainActivity : AppCompatActivity() {
         binding.btnGoAfter.setOnClickListener { goAfterMusic() }
         binding.btnVolumeOnOof.setOnClickListener { configureVolume() }
 
+        binding.sliderMain.addOnChangeListener { slider, value, fromUser ->
+
+            binding.txLeft.text = convertMillisToString(value.toLong())
+            isUserChanging = fromUser
+        }
+
+        binding.sliderMain.addOnSliderTouchListener(object : Slider.OnSliderTouchListener {
+            override fun onStartTrackingTouch(slider: Slider) {
+
+            }
+
+            override fun onStopTrackingTouch(slider: Slider) {
+                mediaPlayer.seekTo(slider.value.toInt())
+            }
+        })
     }
 
     private fun prepareMusic() {
@@ -30,16 +48,38 @@ class MainActivity : AppCompatActivity() {
         mediaPlayer.start()
         isPlaying = true
 
+        binding.sliderMain.valueTo = mediaPlayer.duration.toFloat()
+
+        binding.txRight.text = convertMillisToString(mediaPlayer.duration.toLong())
+
+        val timer = Timer()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                if (!isUserChanging) {
+                    runOnUiThread {
+                        binding.sliderMain.value = mediaPlayer.currentPosition.toFloat()
+                    }
+                }
+            }
+        }, 1000, 1000)
+    }
+
+    private fun convertMillisToString(duration: Long): String {
+
+        val second = duration / 1000 % 60
+        val minute = duration / (1000 * 60) % 60
+
+        return java.lang.String.format(Locale.US, "%02d:%02d", minute, second)
     }
 
     private fun configureMusic() {
 
-        if (isPlaying){
+        if (isPlaying) {
             mediaPlayer.pause()
             binding.btnPlayPause.setImageResource(R.drawable.ic_play)
             isPlaying = false
 
-        }else{
+        } else {
             mediaPlayer.start()
             binding.btnPlayPause.setImageResource(R.drawable.ic_pause)
             isPlaying = true
